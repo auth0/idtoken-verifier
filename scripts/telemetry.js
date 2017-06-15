ac58778452e7d028f1e298fef031bc0dda50f06f
Hernan Zalazar <hernan.zalazar@gmail.com>
2017-06-15 20:48:17 -0300

    Fix release script

--- scripts/telemetry.js
+++ scripts/telemetry.js
@@ -5,8 +5,8 @@ if (process.platform === 'win32') {
 
 var library = require('../package.json');
 var fs = require('fs');
-var telemetry = require('../src/version');
+var telemetry = require('../src/telemetry');
 var execSync = require('child_process').execSync;
 telemetry.version = library.version;
-fs.writeFileSync('src/version.js', `// Generated file by ${process.env['USER']} on ${new Date()};\nmodule.exports = ${JSON.stringify(telemetry, null, 2)};`);
-execSync('git add src/version.js');
+fs.writeFileSync('src/telemetry.js', `// Generated file by ${process.env['USER']} on ${new Date()};\nmodule.exports = ${JSON.stringify(telemetry, null, 2)};`);
+execSync('git add src/telemetry.js');
