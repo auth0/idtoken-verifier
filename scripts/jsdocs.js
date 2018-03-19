db811021dd58ed1edb5adc3bbc095bb27d2be3b2
Luis Deschamps Rudge <luis@luisrudge.net>
2018-03-19 19:28:01 -0300

    rollback script change

--- scripts/jsdocs.js
+++ scripts/jsdocs.js
@@ -1,3 +1,7 @@
+if (process.platform === 'win32') {
+  console.error('Must be run on a Unix OS');
+  process.exit(1);
+}
 
 var library = require('../package.json');
 var execSync = require('child_process').execSync;
