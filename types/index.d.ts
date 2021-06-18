b48b6223295687494e326ced7c1f59c88ee94c85
Steve Hobbs <steve.hobbs.mail@gmail.com>
2021-06-18 11:43:24 +0100

    Run npm update

--- types/index.d.ts
+++ types/index.d.ts
@@ -74,9 +74,60 @@ declare class IdTokenVerifier {
   jwksURI: any;
   maxAge: any;
   __clock: any;
+  /**
+   * @callback verifyCallback
+   * @param {?Error} err error returned if the verify cannot be performed
+   * @param {?object} payload payload returned if the token is valid
+   */
+  /**
+   * Verifies an id_token
+   *
+   * It will validate:
+   * - signature according to the algorithm configured in the verifier.
+   * - if nonce is present and matches the one provided
+   * - if `iss` and `aud` claims matches the configured issuer and audience
+   * - if token is not expired and valid (if the `nbf` claim is in the past)
+   *
+   * @method verify
+   * @param {string} token id_token to verify
+   * @param {string} [requestedNonce] nonce value that should match the one in the id_token claims
+   * @param {verifyCallback} cb callback used to notify the results of the validation
+   */
   verify(token: string, requestedNonce?: string, cb: verifyCallback): any;
   getRsaVerifier(iss: any, kid: any, cb: any): void;
+  /**
+   * @typedef DecodedToken
+   * @type {Object}
+   * @property {Object} header - content of the JWT header.
+   * @property {Object} payload - token claims.
+   * @property {Object} encoded - encoded parts of the token.
+   */
+  /**
+   * Decodes a well formed JWT without any verification
+   *
+   * @method decode
+   * @param {string} token decodes the token
+   * @return {DecodedToken} if token is valid according to `exp` and `nbf`
+   */
   decode(token: string): DecodedToken;
+  /**
+   * @callback validateAccessTokenCallback
+   * @param {Error} [err] error returned if the validation cannot be performed
+   * or the token is invalid. If there is no error, then the access_token is valid.
+   */
+  /**
+   * Validates an access_token based on {@link http://openid.net/specs/openid-connect-core-1_0.html#ImplicitTokenValidation}.
+   * The id_token from where the alg and atHash parameters are taken,
+   * should be decoded and verified before using thisfunction
+   *
+   * @method validateAccessToken
+   * @param {string} access_token the access_token
+   * @param {string} alg The algorithm defined in the header of the
+   * previously verified id_token under the "alg" claim.
+   * @param {string} atHash The "at_hash" value included in the payload
+   * of the previously verified id_token.
+   * @param {validateAccessTokenCallback} cb callback used to notify the results of the validation.
+   */
   validateAccessToken(
     accessToken: any,
     alg: string,
