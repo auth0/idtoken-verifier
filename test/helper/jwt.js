6a81cb956305de5709a58e62db77806d38101908
Steve Hobbs <steve.hobbs@hey.com>
2022-12-23 12:44:49 +0000

    abandon use of crypto module

--- test/helper/jwt.js
+++ test/helper/jwt.js
@@ -1,6 +1,5 @@
 import pem from 'pem';
 import jwt from 'jsonwebtoken';
-import * as crypto from 'crypto';
 
 export const defaultExp = 1482969031; // this is the exp in the defaultToken below 👇
 
@@ -11,12 +10,6 @@ export const defaultToken =
   'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlF6RTROMFpCTTBWRFF6RTJSVVUwTnpJMVF6WTFNelE0UVRrMU16QXdNRUk0UkRneE56RTRSZyJ9.eyJpc3MiOiJodHRwczovL3dwdGVzdC5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NTVkNDhjNTdkNWIwYWQwMjIzYzQwOGQ3IiwiYXVkIjoiZ1lTTmxVNFlDNFYxWVBkcXE4elBRY3VwNnJKdzFNYnQiLCJleHAiOjE0ODI5NjkwMzEsImlhdCI6MTQ4MjkzMzAzMSwibm9uY2UiOiJhc2ZkIn0.PPoh-pITcZ8qbF5l5rMZwXiwk5efbESuqZ0IfMUcamB6jdgLwTxq-HpOT_x5q6-sO1PBHchpSo1WHeDYMlRrOFd9bh741sUuBuXdPQZ3Zb0i2sNOAC2RFB1E11mZn7uNvVPGdPTg-Y5xppz30GSXoOJLbeBszfrVDCmPhpHKGGMPL1N6HV-3EEF77L34YNAi2JQ-b70nFK_dnYmmv0cYTGUxtGTHkl64UEDLi3u7bV-kbGky3iOOCzXKzDDY6BBKpCRTc2KlbrkO2A2PuDn27WVv1QCNEFHvJN7HxiDDzXOsaUmjrQ3sfrHhzD7S9BcCRkekRfD9g95SKD5J0Fj8NA';
 
 const symmetricSecret = 'secret key';
-
-const createSymmetricKey = (secret = symmetricSecret) =>
-  crypto
-    .createHash('sha256')
-    .update(secret)
-    .digest('base64');
 
 const createCertificate = () =>
   new Promise((res, rej) => {
@@ -54,7 +47,7 @@ export const createJWT = (
 ) => {
   return createCertificate().then(cert => {
     const key =
-      options.algorithm === 'HS256' ? createSymmetricKey() : cert.serviceKey;
+      options.algorithm === 'HS256' ? symmetricSecret : cert.serviceKey;
 
     return jwt.sign(payload, key, {
       algorithm: 'RS256',
